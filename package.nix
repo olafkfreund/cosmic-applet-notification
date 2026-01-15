e19d408c57f706903c86018b1d7105319080105e
olafkfreund <olaf.loken@gmail.com>
2026-01-15 12:24:29 +0000

    fix(nix): Add output hashes for git dependencies
    Add outputHashes for all git dependencies in Cargo.lock to fix
    Nix build workflow. Using lib.fakeHash initially to obtain real
    hashes from build errors.
    
    Addresses GitHub Actions Nix Build workflow failures.

--- package.nix
+++ package.nix
@@ -22,11 +22,51 @@ rustPlatform.buildRustPackage rec {
 
   cargoLock = {
     lockFile = ./Cargo.lock;
-    # libcosmic and other git dependencies need output hashes
-    # These will be automatically determined during first build
-    # outputHashes = {
-    #   "libcosmic-0.1.0" = lib.fakeHash;
-    # };
+    # Git dependencies require output hashes for vendoring
+    # Using lib.fakeHash initially - build will provide real hashes
+    outputHashes = {
+      "accesskit-0.16.0" = lib.fakeHash;
+      "accesskit_atspi_common-0.9.0" = lib.fakeHash;
+      "accesskit_consumer-0.24.0" = lib.fakeHash;
+      "accesskit_macos-0.17.0" = lib.fakeHash;
+      "accesskit_unix-0.12.0" = lib.fakeHash;
+      "accesskit_windows-0.22.0" = lib.fakeHash;
+      "accesskit_winit-0.22.0" = lib.fakeHash;
+      "atomicwrites-0.4.2" = lib.fakeHash;
+      "clipboard_macos-0.1.0" = lib.fakeHash;
+      "clipboard_wayland-0.2.2" = lib.fakeHash;
+      "clipboard_x11-0.4.2" = lib.fakeHash;
+      "cosmic-client-toolkit-0.1.0" = lib.fakeHash;
+      "cosmic-config-0.1.0" = lib.fakeHash;
+      "cosmic-config-derive-0.1.0" = lib.fakeHash;
+      "cosmic-freedesktop-icons-0.4.0" = lib.fakeHash;
+      "cosmic-panel-config-0.1.0" = lib.fakeHash;
+      "cosmic-protocols-0.1.0" = lib.fakeHash;
+      "cosmic-settings-daemon-0.1.0" = lib.fakeHash;
+      "cosmic-text-0.16.0" = lib.fakeHash;
+      "cosmic-theme-0.1.0" = lib.fakeHash;
+      "dnd-0.1.0" = lib.fakeHash;
+      "dpi-0.1.1" = lib.fakeHash;
+      "iced-0.14.0-dev" = lib.fakeHash;
+      "iced_accessibility-0.1.0" = lib.fakeHash;
+      "iced_core-0.14.0-dev" = lib.fakeHash;
+      "iced_futures-0.14.0-dev" = lib.fakeHash;
+      "iced_glyphon-0.6.0" = lib.fakeHash;
+      "iced_graphics-0.14.0-dev" = lib.fakeHash;
+      "iced_renderer-0.14.0-dev" = lib.fakeHash;
+      "iced_runtime-0.14.0-dev" = lib.fakeHash;
+      "iced_tiny_skia-0.14.0-dev" = lib.fakeHash;
+      "iced_wgpu-0.14.0-dev" = lib.fakeHash;
+      "iced_widget-0.14.0-dev" = lib.fakeHash;
+      "iced_winit-0.14.0-dev" = lib.fakeHash;
+      "libcosmic-0.1.0" = lib.fakeHash;
+      "mime-0.1.0" = lib.fakeHash;
+      "smithay-clipboard-0.8.0" = lib.fakeHash;
+      "softbuffer-0.4.1" = lib.fakeHash;
+      "window_clipboard-0.4.1" = lib.fakeHash;
+      "winit-0.30.5" = lib.fakeHash;
+      "xdg-shell-wrapper-config-0.1.0" = lib.fakeHash;
+    };
   };
 
   nativeBuildInputs = [
